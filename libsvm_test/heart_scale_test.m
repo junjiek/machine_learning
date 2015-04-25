717f4207b92e40055658ca095a255fb809827465
柯均洁 <kjj3300@163.com>
2015-04-25 19:41:12 +0800

    add ex7

--- libsvm_test/heart_scale_test.m
+++ libsvm_test/heart_scale_test.m
@@ -1,3 +1,5 @@
+clear;
+clc;
 load heart_scale
 [N D] = size(heart_scale_inst);
 
@@ -16,3 +18,56 @@ testLabel = heart_scale_label(testIndex==1,:);
 model = svmtrain(trainLabel, trainData, '-c 1 -g 0.07 -b 1');
 % Use the SVM model to classify the data
 [predict_label, accuracy, prob_values] = svmpredict(testLabel, testData, model, '-b 1'); % run the SVM model on the test data
+% ================================
+% ===== Showing the results ======
+% ================================
+
+% Assign color for each class
+% colorList = generateColorList(2); % This is my own way to assign the color...don't worry about it
+colorList = prism(100);
+
+% true (ground truth) class
+trueClassIndex = zeros(N,1);
+trueClassIndex(heart_scale_label==1) = 5; 
+trueClassIndex(heart_scale_label==-1) = 50;
+colorTrueClass = colorList(trueClassIndex,:);
+% result Class
+resultClassIndex = zeros(length(predict_label),1);
+resultClassIndex(predict_label==1) = 5; 
+resultClassIndex(predict_label==-1) = 50;
+colorResultClass = colorList(resultClassIndex,:);
+
+% Reduce the dimension from 13D to 2D
+distanceMatrix = pdist(heart_scale_inst,'euclidean');
+newCoor = mdscale(distanceMatrix,2);
+
+% Plot the whole data set
+x = newCoor(:,1);
+y = newCoor(:,2);
+patchSize = 30; %max(prob_values,[],2);
+colorTrueClassPlot = colorTrueClass;
+figure; scatter(x,y,patchSize,colorTrueClassPlot,'filled');
+title('whole data set');
+
+% Plot the test data
+x = newCoor(testIndex==1,1);
+y = newCoor(testIndex==1,2);
+patchSize = 80*max(prob_values,[],2);
+colorTrueClassPlot = colorTrueClass(testIndex==1,:);
+figure; hold on;
+scatter(x,y,2*patchSize,colorTrueClassPlot,'o','filled'); 
+scatter(x,y,patchSize,colorResultClass,'o','filled');
+% Plot the training set
+x = newCoor(trainIndex==1,1);
+y = newCoor(trainIndex==1,2);
+patchSize = 30;
+colorTrueClassPlot = colorTrueClass(trainIndex==1,:);
+scatter(x,y,patchSize,colorTrueClassPlot,'o');
+title('classification results');
+% The unfilled markers represent data instance from the train set.
+% The filled markers represent data instance from the test set,
+% and filled color represents the class label assigned by SVM whereas
+% the edge color represents the true (ground-truth) label.
+% The marker size of the test set represents the probability that the
+% sample instance is assigned with its corresponding class label;
+% the bigger, the more confidence.   
